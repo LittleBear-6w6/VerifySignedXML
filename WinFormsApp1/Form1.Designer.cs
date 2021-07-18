55cfa367f7ebfccc55edbc85e881f69b8ed533d3
LittleBear-6w6 <57203646+LittleBear-6w6@users.noreply.github.com>
2021-07-18 10:57:31 +0900

    upload src

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace WinFormsApp1
+{
+	partial class XML電子署名検証ツール
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.openFileButton = new System.Windows.Forms.Button();
+			this.FileName = new System.Windows.Forms.TextBox();
+			this.verfyXML = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// openFileButton
+			// 
+			this.openFileButton.Location = new System.Drawing.Point(468, 60);
+			this.openFileButton.Name = "openFileButton";
+			this.openFileButton.Size = new System.Drawing.Size(290, 77);
+			this.openFileButton.TabIndex = 0;
+			this.openFileButton.Text = "ファイル開く";
+			this.openFileButton.UseVisualStyleBackColor = true;
+			this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+			// 
+			// FileName
+			// 
+			this.FileName.Enabled = false;
+			this.FileName.Location = new System.Drawing.Point(41, 83);
+			this.FileName.Name = "FileName";
+			this.FileName.Size = new System.Drawing.Size(406, 23);
+			this.FileName.TabIndex = 1;
+			// 
+			// verfyXML
+			// 
+			this.verfyXML.Location = new System.Drawing.Point(468, 201);
+			this.verfyXML.Name = "verfyXML";
+			this.verfyXML.Size = new System.Drawing.Size(289, 99);
+			this.verfyXML.TabIndex = 2;
+			this.verfyXML.Text = "検証";
+			this.verfyXML.UseVisualStyleBackColor = true;
+			this.verfyXML.Click += new System.EventHandler(this.verfyXML_Click);
+			// 
+			// XML電子署名検証ツール
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.verfyXML);
+			this.Controls.Add(this.FileName);
+			this.Controls.Add(this.openFileButton);
+			this.Name = "XML電子署名検証ツール";
+			this.Text = "XML電子署名検証ツール";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button openFileButton;
+		private System.Windows.Forms.TextBox FileName;
+		private System.Windows.Forms.Button verfyXML;
+	}
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+
+
+
+namespace WinFormsApp1
+{
+	public partial class XML電子署名検証ツール : Form
+	{
+		public XML電子署名検証ツール()
+		{
+			InitializeComponent();
+		}
+
+		private void openFileButton_Click (object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.InitialDirectory = @"C:\";
+			ofd.Title = "開くファイルを選択してください";
+			ofd.RestoreDirectory = true;
+			ofd.CheckFileExists = true;
+			ofd.CheckPathExists = true;
+
+			if(ofd.ShowDialog() == DialogResult.OK)
+			{
+				FileName.Text = ofd.FileName;
+			}
+		}
+
+		private void verfyXML_Click(object sender, EventArgs e)
+		{
+			verfy(FileName.Text);
+		}
+
+		public static void verfy(string signedXmlFilename)
+		{
+			try
+			{
+				XmlDocument xmlDoc = new XmlDocument();
+				xmlDoc.PreserveWhitespace = true;
+				xmlDoc.Load(signedXmlFilename);
+
+				bool result = VerifyXml(xmlDoc);
+				if (result)
+				{
+					MessageBox.Show("検証正常！", "検証結果", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+				else
+				{
+					MessageBox.Show("検証異常！", "検証結果", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.Message, "異常終了", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		public static Boolean VerifyXml(XmlDocument doc)
+		{
+			SignedXml signedXml = new SignedXml(doc);
+			XmlNodeList nodeList = doc.GetElementsByTagName("Signature");
+			if(nodeList.Count <= 0)
+			{
+				throw new CryptographicException("検証エラー！<Signature>タグがありません。");
+			}
+			if (nodeList.Count >= 2)
+			{
+				throw new CryptographicException("検証エラー！<Signature>タグが2個以上あります。");
+			}
+
+			signedXml.LoadXml((XmlElement)nodeList[0]);
+			return signedXml.CheckSignature();
+		}
+	}
+}
